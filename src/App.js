39246b5b3edd3ab0a396c801fdb17ec444aa91a3
Fernanda <mangorifernanda@gmail.com>
2023-03-26 21:26:17 +0700

    step 2 done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Routes from './router';
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,32 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../pages/Home';
 import Splash from '../pages/Splash';
+import Akun from '../pages/Akun';
+import Pesanan from '../pages/Pesanan';
 
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const MainApp = () => {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="Home" component={Home} />
+            <Tab.Screen name="Akun" component={Akun} />
+            <Tab.Screen name="Pesanan" component={Pesanan} />
+        </Tab.Navigator>
+    )
+}
 
 const Routes = () => {
     return (
-        <Stack.Navigator initialRouteName='Splash'>
-  
-            <Stack.Screen name="Home" component={Home} />
+        <Stack.Navigator initialRouteName='MainApp'>
+            <Stack.Screen name="MainApp" component={MainApp} />
             <Stack.Screen name="Splash" component={Splash} />
+
         </Stack.Navigator >
     )
 }
