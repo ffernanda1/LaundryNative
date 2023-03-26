ab7201891d67fd8e4671d47d48be0c2f8844b52b
Fernanda <mangorifernanda@gmail.com>
2023-03-26 19:34:15 +0700

    step2

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const App = () => {
+  return (
+    <View>
+      <Text>hello world</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
