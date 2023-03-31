867d2ac5b844b24c154c62a80072aed748225e9a
Fernanda <mangorifernanda@gmail.com>
2023-03-31 09:05:34 +0700

    progress image

---
+++ src/assets/images/index.js
@@ -0,0 +1,4 @@
+import Logo from './logo.png';
+import Splash from './background.png';
+
+export {Logo, Splash} 
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from './Home'
+import Akun from './Akun'
+import Pesanan from './Pesanan'
+import Splash from './Splash'
+
+export { Home, Akun, Splash, Pesanan }
