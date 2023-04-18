0c8bb01cf98078aa41df6b64e7f1c25afaf0551b
Fernanda <mangorifernanda@gmail.com>
2023-04-18 16:55:21 +0700

    saldo

--- src/component/index.js
+++ src/component/index.js
@@ -1,3 +1,4 @@
 import BottomNavigation from "./BottomNavigation";
+import Saldo from "./Saldo"
 
-export { BottomNavigation }
+export { BottomNavigation, Saldo }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -14,6 +14,7 @@ const Home = () => {
           <Text style={styles.usernames}>Fernanda</Text>
         </View>
       </ImageBackground>
+      <Saldo/>
     </View>
   )
 }
