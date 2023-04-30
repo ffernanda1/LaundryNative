946675536227a75da5b6f8be54cebd780e0721c4
Fernanda <mangorifernanda@gmail.com>
2023-04-30 14:04:07 +0700

    layanan

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -6,6 +6,13 @@ import Woman from "./006-charwoman.svg";
 import WomanActive from "./006-charwomanActive.svg";
 import AddSaldo from "./bill_1.svg";
 import GetPoint from "./outcome_1.svg";
+import Clothes from "./009-clean_clothes_1.svg";
+import Dress from "./010-dress_1.svg";
+import Laundry from "./011-laundry_1.svg";
+import Iron from "./021-iron_1.svg";
+import Truck from "./036-truck_1.svg";
+import Scale from "./041-scale_1.svg";
+
 
 export { 
     House, 
@@ -15,6 +22,11 @@ export {
     Woman, 
     WomanActive, 
     AddSaldo, 
-    GetPoint 
-
+    GetPoint,
+    Clothes,
+    Dress,
+    Laundry,
+    Iron,
+    Truck,
+    Scale,
 };
