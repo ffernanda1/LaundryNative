c7d88c640de71da68c093301b7094b8aeddc076a
Fernanda <mangorifernanda@gmail.com>
2023-05-02 19:34:24 +0700

    Layanan

---
+++ src/component/Layanan/index.js
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
